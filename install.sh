1cf3ec0dd885deac23fa77fba7ff171bd062f314
hotsno <71658949+hotsno@users.noreply.github.com>
2024-01-27 11:58:47 -0800

    Fix p10k install

--- install.sh
+++ install.sh
@@ -121,7 +121,7 @@ sudo ln -sf /usr/share/fonctconfig/conf.avail/75-twemoji.conf /etc/fonts/conf.d/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 yay -S ttf-meslo-nerd-font-powerlevel10k
 
 xdg-settings set default-web-browser firefox.desktop
